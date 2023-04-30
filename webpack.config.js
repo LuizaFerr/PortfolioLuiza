211cb0370509ed981429b4f982c1cf41cc3fd168
LuizaFerr <luizaferrair234@gmail.com>
2023-04-30 22:12:50 +0200

    html into docs

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,45 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development', // o 'production'
-    entry: './src/index.tsx',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-              },
-              {
-                test: /\.css$/,
-                use: [
-                  'style-loader',
-                  'css-loader',
-                  'postcss-loader'
-                ]
-              },
-              {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                  {
-                    loader: 'file-loader',
-                  },
-                ],
-              },
-              {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-              },
-            ],
+  mode: 'development', // o 'production'
+  entry: './src/index.tsx',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
           },
-    
-   
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'docs', 'index.html'),
+    }),
+  ],
 };
